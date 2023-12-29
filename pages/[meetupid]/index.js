b9f0791d01998ee5de247d375699f9ccf9c6c3f9
Naveen5676 <sample.first@yahoo.com>
2023-12-29 20:04:06 +0530

    changed index

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -3,7 +3,7 @@ import Meetupdetail from "../../components/meetups/MeetupDetail";
 export default function Meetupdetails() {
   return (
     <Meetupdetail
-      image="https://img.freepik.com/free-vector/city-skyline-concept-illustration_114360-8923.jpg"
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
       title="A First meetup"
       address="some street 5 , some city"
       description="the meetup description"
