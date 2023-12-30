9e260ec4e6c4d0fe36cbd71e172cb75d0269e389
Naveen5676 <sample.first@yahoo.com>
2023-12-30 18:47:45 +0530

    img tag

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,11 +1,11 @@
 import { Fragment } from "react";
 import classes from './MeetupDetail.module.css'
-import Image from 'next/image';
+
 
 export default function Meetupdetail(props) {
   return(
   <section className={classes.detail}>
-    <Image alt='image' src={props.image} />
+    <img alt='image' src={props.image} />
     <h1>{props.title}</h1>
     <address>{props.address}</address>
     <p>{props.description}</p>
